3045a780846fd39d5b50b65d7b2fef40d7b498ab
Muhammad Haris Awan <111harisawan@gmail.com>
2026-09-05 05:07:51 +0500

    feat: initialize frontend project structure with Tailwind CSS and custom design system components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,6 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/noto-nastaliq-urdu/400.css";
-import "@fontsource/noto-nastaliq-urdu/600.css";
-import "@fontsource/noto-nastaliq-urdu/700.css";
 import App from "./App";
 import "./index.css";
 
